d72d89cbf65f27e4fb0823c5b699f0db03cfeff9
Nicolas Scravaglieri <nicolasscravaglieri@MacBook-Pro-de-Nicolas.local>
2024-10-24 20:04:26 -0300

    Clase 24/10

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-class Telefono:
-    red_telefonos={}
+class Celular:
     
     def __init__(self, identificacion, nombre, modelo, sistema_operativo, version, RAM, almacenamiento, num_telefonico) :
         #falta agregar validadores
@@ -12,21 +11,31 @@ class Telefono:
         self.almacenamiento=almacenamiento
         self.num_telefonico=num_telefonico
         self.encendido = False #indica si esta encendido o apagado
-        self.desbloqueado=False #indica si esta desbloqueado el telefono
-        self.red_movil=False #indica si esta en la red
-        self.datos_moviles= True #el telefono viene con datos moviles
+        self.desbloqueado=False #indica si esta desbloqueado el celular
+        self.codigo=None
+        
+        contactos = Contactos()
+        
+        #creamos un diccionario para reflejar cada aplicacion y poder identificarla por su nombre
+        self.apps={"contactos": contactos,
+                   "sms": SMS(contactos), 
+                   "email": Email(),
+                   "telefono": Telefono(contactos),
+                   'app store': App_Store(self), #accede al celular para modificar las apps dentro
+                    'configuracion': Configuracion(self) #accede a toda la configuracion del celular
+                   }
         
     def enceder_apagar(self,cls):
         self.encendido = not self.encendido
         if self.encendido:
-            #una vez encendido, se agrega el telefono a la red
+            #una vez encendido, se agrega el celular a la red
             self.red_movil=True
-            cls.red_telefonos[self.num_telefonico]=self #su key va a ser el numero telefonico
+            #ACA deberia CONECTAR CON LA CENTRAL
             print("El celular se ha prendido")
         else:
             #una vez apagado, se elimina de la red
             self.red_movil=False
-            cls.red_telefonos.pop(self.num_telefonico)
+            #ACA deberia CONECTAR CON LA CENTRAL
             print("Has apagado el celular")
         
     def bloq_desbloq(self):
@@ -36,27 +45,102 @@ class Telefono:
             print("El celular se ha desbloqueado")
         else:
             print("Has bloqueado el celular")
+            
+        
+    def abrir_app(self):
+        aplicacion=input(f"Que aplicacicion desea abrir: {self.apps.keys}")
+        self.apps[aplicacion].abrir()
         
     
-    def app_telefono(self):
+        
+class Aplicacion():
+    def __init__ (self):
         pass
-    
-    def app_contactos(self):
-        pass
-    
-    def app_mensajes(self):
-        pass
-    
-    def app_mail(self):
-        pass
-    
-    def app_store(self):
-        pass
-    
-    # hay que agregar la funcionalidad de desactivar la red movil 
-    def app_configuracion(self):
-        pass
-    
-    
     
         
+class Contactos(Aplicacion):
+    def __init__(self):
+        self.lista_de_contactos={}
+    
+    def agendar(self,celular: Celular):
+        #tanto para modificar como para agregar contactos, se usa el mismo key por ende es la misma funcionalidad que reemplaza el value
+        self.lista_de_contactos[celular.nombre]=celular
+        
+
+
+class SMS(Aplicacion):
+    def __init__(self,contactos):
+        self.bandeja_sms=[] #pila para ver cual llega primero 
+        self.contactos=contactos
+        pass
+    
+   
+        
+    def enviar_mensaje(self,receptor: str):
+        pass 
+    
+    def ver_bandeja_sms(self):
+        pass 
+    
+    def eliminar_mensajes(self):
+        pass
+    
+        
+    
+    
+class Telefono(Aplicacion):
+    def __init__(self,contactos):
+        self.historial_llamadas=[] #Cola para ver cual llega primero 
+        self.en_llamada=False #se activa si estas en llamada, no podes estar en dos llamadas al mismo tiempo
+        self.contactos=contactos
+    
+
+    def llamar(self):
+        pass
+    
+    def recibir_llamado(self):
+        pass
+    
+    def terminar_llamada(self):
+        pass 
+    
+    def ver_historial_llamadas(self):
+        pass
+    
+    
+
+class Email(Aplicacion):
+    def __init__(self):
+        self.bandeja_email=[] #pila
+        
+    def ver_bandeja_mails(self,filtrar_leido=False):
+        pass
+        
+class App_Store(Aplicacion):
+    def __init__(self,celular):
+        self.celular=celular
+        
+    def descargar_app(self):
+        self.celular.apps[input("Que app quiere descargar?")] = Aplicacion() 
+        #faltan validaciones
+    
+
+class Configuracion(Aplicacion):
+    def __init__(self,celular):
+        self.celular=celular
+
+    def cambiar_nombre(self,nuevo_nombre: str):
+        self.celular.nombre=nuevo_nombre
+        
+    def cambiar_codigo(self,nuevo_codigo: int):
+        if self.celular.codigo == None or self.celular.codigo == int(input('ingrese el codigo actual: ')): #chequear
+            self.celular.codigo=nuevo_codigo
+            
+    def activar_red_movil(self):
+        pass 
+    
+    def desactivar_red_movil(self):
+        pass
+    
+    def datos(self):
+        pass
